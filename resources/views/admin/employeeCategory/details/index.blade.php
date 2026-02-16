a08967b8fda0045fbb764e69acfb827df2da7145
batistalando7 <batistalando7@gmail.com>
2026-02-16 12:45:26 +0100

    Categoria e curso já padronizados

--- resources/views/admin/employeeCategory/details/index.blade.php
+++ resources/views/admin/employeeCategory/details/index.blade.php
@@ -1,49 +1,70 @@
-@extends("layouts.merge.admin")
-@section("title", "Detalhes da Categoria de Funcionário")
-@section("content")
+@extends('layouts.merge.admin')
+@section('title', 'Detalhes da Categoria de Funcionário')
+@section('content')
 
-  <div class="card my-4 shadow">
-    <div class="card-header bg-secondary text-white d-flex justify-content-between align-items-center">
-      <span><i class="fas fa-info-circle me-2"></i>Detalhes da Categoria de Funcionário</span>
-      <a href="{{ route("admin.employeeCategories.index") }}" class="btn btn-outline-light btn-sm" title="Voltar">
-        <i class="fas fa-arrow-left"></i>
-      </a>
+    <!-- [ page-header ] start -->
+    <div class="page-header">
+        <div class="page-header-left d-flex align-items-center">
+            <div class="page-header-title">
+                <h5 class="m-b-10">Categorias</h5>
+            </div>
+            <ul class="breadcrumb">
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                <li class="breadcrumb-item">Detalhes da Categoria</li>
+            </ul>
+        </div>
+        <div class="page-header-right ms-auto">
+            <div class="page-header-right-items">
+                <div class="d-flex d-md-none">
+                    <a href="javascript:void(0)" class="page-header-right-close-toggle">
+                        <i class="feather-arrow-left me-2"></i>
+                        <span>Back</span>
+                    </a>
+                </div>
+                <div class="d-flex align-items-center gap-2 page-header-right-items-wrapper">
+                    <a href="{{ route('admin.employeeCategories.index') }}" class="btn btn-outline-secondary">
+                        <i class="feather-list me-2"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="card-body">
-      <table class="table table-striped table-bordered">
-        <tbody>
-          <tr>
-            <th>ID</th>
-            <td>{{ $employeeCategory->id }}</td>
-          </tr>
-          <tr>
-            <th>Nome da Categoria</th>
-            <td>{{ $employeeCategory->name }}</td>
-          </tr>
-          <tr>
-            <th>Criado em</th>
-            <td>{{ \Carbon\Carbon::parse($employeeCategory->created_at)->format("d/m/Y H:i:s") }}</td>
-          </tr>
-          <tr>
-            <th>Atualizado em</th>
-            <td>{{ \Carbon\Carbon::parse($employeeCategory->updated_at)->format("d/m/Y H:i:s") }}</td>
-          </tr>
-        </tbody>
-      </table>
-      <div class="d-flex justify-content-end">
-        <a href="{{ route("admin.employeeCategories.edit", $employeeCategory->id) }}" class="btn btn-info me-2">
-          <i class="fas fa-pencil"></i> Editar
-        </a>
-        <form action="{{ route("admin.employeeCategories.destroy", $employeeCategory->id) }}" method="POST"
-          onsubmit="return confirm(" Tem certeza que deseja excluir esta categoria?");">
-          @csrf
-          @method("DELETE")
-          <button type="submit" class="btn btn-danger">
-            <i class="fas fa-trash"></i> Excluir
-          </button>
-        </form>
-      </div>
+    <!-- [ page-header ] end -->
+    <div class="main-content">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card stretch stretch-full">
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-hover">
+                                <tbody>
+                                    <tr>
+                                        <th>ID</th>
+                                        <td>{{ $employeeCategory->id }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Nome da Categoria</th>
+                                        <td>{{ $employeeCategory->name }}</td>
+                                    </tr>
+                                    <tr>
+                                        <th>Criado em</th>
+                                        <td>{{ \Carbon\Carbon::parse($employeeCategory->created_at)->format('d/m/Y H:i:s') }}
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>Atualizado em</th>
+                                        <td>{{ \Carbon\Carbon::parse($employeeCategory->updated_at)->format('d/m/Y H:i:s') }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
+
 
 @endsection
